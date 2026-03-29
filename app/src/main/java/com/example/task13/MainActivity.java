389700fa555169ada3e8f0caeaba242edc699da2
nitzandamri2 <nitzandamri@gmail.com>
2026-03-29 21:51:11 +0300

    adiing main

--- app/src/main/java/com/example/task13/MainActivity.java
+++ app/src/main/java/com/example/task13/MainActivity.java
@@ -1,24 +1,60 @@
 package com.example.task13;
 
+import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    // הגדרת המשתנים
+    LinearLayout mainLayout;
+    Switch mySwitch;
+    RadioGroup myRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // קישור לרכיבים מה-XML
+        mainLayout = findViewById(R.id.mainLayout);
+        mySwitch = findViewById(R.id.mySwitch);
+        myRadioGroup = findViewById(R.id.myRadioGroup);
+    }
+
+    // פונקציה שנקראת כשלוחצים על אחד מכפתורי הרדיו
+    public void checkRadio(View v) {
+        // החלפת צבע רקע במסך מיד בכל לחיצה על לחצן שנבחר ב-RG (כשהסוויץ' דולק)
+        if (mySwitch.isChecked()) {
+            changeBackgroundColor();
+        }
+    }
+
+    // פונקציה שנקראת כשלוחצים על הכפתור למטה
+    public void buttonClick(View v) {
+        // החלפת צבע רקע במסך רק כאשר נלחץ לחצן ההפעלה (כשהסוויץ' כבוי)
+        if (!mySwitch.isChecked()) {
+            changeBackgroundColor();
+        }
+    }
+
+    // פונקציית עזר שבודקת מה נבחר ומשנה את הצבע
+    public void changeBackgroundColor() {
+        int checkedId = myRadioGroup.getCheckedRadioButtonId();
+
+        if (checkedId == R.id.rbRed) {
+            mainLayout.setBackgroundColor(Color.RED);
+        } else if (checkedId == R.id.rbGreen) {
+            mainLayout.setBackgroundColor(Color.GREEN);
+        } else if (checkedId == R.id.rbBlue) {
+            mainLayout.setBackgroundColor(Color.BLUE);
+        } else if (checkedId == R.id.rbYellow) {
+            mainLayout.setBackgroundColor(Color.YELLOW);
+        }
     }
 }
